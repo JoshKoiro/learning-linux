f240a3dd92b1518139daa0f05399f811992cebf2
JoshKoiro <josh.koiro@gmail.com>
2025-03-27 16:15:55 -0400

    added --noconfirm flag to remote y/n questions

--- scripts-scratchpad/install-tools.sh
+++ scripts-scratchpad/install-tools.sh
@@ -2,44 +2,44 @@
 
 # Necessary Packages
 
-sudo pacman -S btop
-sudo pacman -S curl
-sudo pacman -S wget
-sudo pacman -S fzf
-sudo pacman -S neofetch
-sudo pacman -S fastfetch
-sudo pacman -S ffmpeg
-sudo pacman -S kitty
-sudo pacman -S nodejs
-sudo pacman -S npm
-sudo pacman -S nvtop
-sudo pacman -S unzip
-sudo pacman -S yazi
-sudo pacman -S eza
+sudo pacman -S btop --noconfirm
+sudo pacman -S curl --noconfirm
+sudo pacman -S wget --noconfirm
+sudo pacman -S fzf --noconfirm
+sudo pacman -S neofetch --noconfirm
+sudo pacman -S fastfetch --noconfirm
+sudo pacman -S ffmpeg --noconfirm
+sudo pacman -S kitty --noconfirm
+sudo pacman -S nodejs --noconfirm
+sudo pacman -S npm --noconfirm
+sudo pacman -S nvtop --noconfirm
+sudo pacman -S unzip --noconfirm
+sudo pacman -S yazi --noconfirm
+sudo pacman -S eza --noconfirm
 
 # Package Managers
-sudo pacman -S yay
-sudo pacman -S flatpak
+sudo pacman -S yay --noconfirm
+sudo pacman -S flatpak --noconfirm
 
 # Applications
 
     # install zen via flatpak
-    flatpak install flathub app.zen_browser.zen
+    flatpak install flathub app.zen_browser.zen --noconfirm
 
     # install discord
-    flatpak install flathub com.discordapp.Discord
+    flatpak install flathub com.discordapp.Discord --noconfirm
 
     # install telegram
-    flatpak install flathub org.telegram.desktop
+    flatpak install flathub org.telegram.desktop --noconfirm
 
     # install spotify
-    flatpak install flathub com.spotify.Client
+    flatpak install flathub com.spotify.Client --noconfirm
 
     # install vivaldi
-    flatpak install flathub com.vivaldi.Vivaldi
+    flatpak install flathub com.vivaldi.Vivaldi --noconfirm
 
 # Developer Tools
-sudo pacman -S git
-sudo pacman -S nodejs
-sudo pacman -S npm
-sudo pacman -S neovim
+sudo pacman -S git --noconfirm
+sudo pacman -S nodejs --noconfirm
+sudo pacman -S npm --noconfirm
+sudo pacman -S neovim --noconfirm
